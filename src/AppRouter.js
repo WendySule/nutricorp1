a535d5f1e20c911640a881b447b92583055cd76c
Denisse Marinel Montalvo Castillo <marinel.casti@gmail.com>
2021-07-12 17:15:28 -0500

    ADD: functionality of login

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -3,7 +3,7 @@ import {
     BrowserRouter as Router,
     Switch,
     Route} from 'react-router-dom';
-import {Loggin} from './components/loggin'
+import Loggin from './components/Loggin';
 
 export const AppRouter = () => {
     return(
