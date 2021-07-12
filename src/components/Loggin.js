6cfad9c6de9436497567543ab0c0fbce8d518d5a
Denisse Marinel Montalvo Castillo <marinel.casti@gmail.com>
2021-07-12 17:15:28 -0500

    ADD: functionality of login

---
+++ src/components/Loggin.js
@@ -0,0 +1,118 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Form } from "react-bootstrap";
+import { logInAuth } from "../services/authService";
+import "../styles/loggin.css";
+
+const emptyForm = {
+  email: "",
+  password: "",
+};
+
+const Loggin = ({ history }) => {
+  const [form, setForm] = useState(emptyForm);
+  const [error, setError] = useState(false);
+  const [remenber, setRemenber] = useState(false);
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleCheck = (e) => {
+    setRemenber(!remenber);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(` ya exito `);
+    console.log(form.email);
+    logInAuth(form.email, form.password)
+      .then((res) => {
+        console.log(`holas email`);
+        console.log(res);
+        if (remenber) {
+          localStorage.setItem("user", JSON.stringify({...form, remenber}));
+        } else {
+          localStorage.removeItem("user");
+        }
+        history.push("/dashboard");
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(true);
+        setForm(emptyForm);
+      });
+  };
+
+  const startSessionWithLocalstorage = () =>{
+    const user = JSON.parse(localStorage.getItem("user"))
+    console.log(user);
+    if(!user){
+        setForm(emptyForm)
+        setRemenber(false)
+    }else{
+        setForm(user)
+        setRemenber(true)
+    }
+    
+
+  }
+
+  useEffect(()=>{
+      startSessionWithLocalstorage()
+      
+  },[])
+
+  return (
+    <section className="container-center">
+      <img src="../assets/image/logo.png" className="logo" alt="logo" />
+      <Form className="xs" onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Control
+            value={form.email}
+            name="email"
+            onChange={handleChange}
+            id="input-email"
+            type="email"
+            placeholder="Correo electronico"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Control
+            value={form.password}
+            name="password"
+            onChange={handleChange}
+            id="input-password"
+            type="password"
+            placeholder="Contraseña"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check
+            type="checkbox"
+            label="Recuerdame"
+            checked={remenber}
+            onChange={handleCheck}
+            //   name="remenber"
+            //   value={remenber}
+          />
+          {error ? (
+            <p className="danger">Usuario o contraseña incorrecto </p>
+          ) : null}
+        </Form.Group>
+        <section className="container-center">
+          <button className="button-red" type="submit">
+            Ingresar
+          </button>
+        </section>
+      </Form>
+    </section>
+  );
+};
+
+export default Loggin;
---
+++ src/environments/firebase-config.js
@@ -0,0 +1,30 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const firebaseConfig = { apiKey: "AIzaSyBtyIUrqeAg6AkgIZzVccHcG4rb-ZUriDo", authDomain: "credicorp01-d7138.firebaseapp.com", databaseURL: "https://credicorp01-d7138-default-rtdb.firebaseio.com", projectId: "credicorp01-d7138", storageBucket: "credicorp01-d7138.appspot.com", messagingSenderId: "223031258472", appId: "1:223031258472:web:c2143bc1a296b1fb413add", measurementId: "G-0X00CYK725" }; 
+
+// Initialize Firebase 
+
+export const fb = firebase.initializeApp(firebaseConfig); 
+// export const db = fb.firestore()
+// export const auth1 = fb.auth()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
