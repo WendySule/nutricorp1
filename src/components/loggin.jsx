f57e8105abcd6f359ed7c8f713923482d0bcadb8
mairelis-montilla <mairelismontilla@gmail.com>
2021-07-12 11:57:32 -0500

    layout login

--- src/components/loggin.jsx
+++ src/components/loggin.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {Form, Button} from 'react-bootstrap';
 
-function loggin() {
+export function loggin() {
   return (
-    <div>
-      hola es el loggin
-    </div>
+<Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Correo</Form.Label>
+    <Form.Control type="email" placeholder="Correo electronico" />
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Contraseña</Form.Label>
+    <Form.Control type="password" placeholder="Contraseña" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Recuerdame" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
   )
 }
 
-export default loggin
+
