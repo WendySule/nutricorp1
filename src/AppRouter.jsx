771451f465fc38e61a60cf8fedd8fb8dedb1c0de
mairelis-montilla <mairelismontilla@gmail.com>
2021-07-12 10:28:17 -0500

    add AppRouter

---
+++ src/AppRouter.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route} from 'react-router-dom';
+
+export const AppRouter = () => {
+    return(
+        <Router>
+            <Switch>
+                <Route path={'/'} exact component={} />
+            </Switch>
+        </Router>
+    )
+}
+
