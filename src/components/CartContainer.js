7d3c26fba6025e22351d3131c26a986d4d8bcdd0
wendyhachilla <wendyhachilla2@gmail.com>
2021-07-13 16:24:05 -0500

    CartAdvance

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import vector from '../assets/image/vector.svg'
+import trash from '../assets/image/trash.svg'
 import "../styles/CartStyle.css"
-
 
 
 
 const CartContainer = () => {
 
     return(
+       <> 
         <section>
             <section >
                 <section className="vector-icon" >  
                     <a href="/home">
-                    <img src={vector} alt="cart-icon" className="vector-icon2" 
+                    <img src={vector} alt="vector-icon" className="vector-icon2" 
                     />
                     </a>
                 </section>
@@ -27,10 +28,51 @@ const CartContainer = () => {
                 </section>
             </section>
             <section>
-                <section>contenedor</section>
-
+                <section className="cardContainer">
+                    <article className="card-section">
+                        <img className="card-img" alt="img"></img>
+                        <div className="card-section-sum">
+                            <button className="button-rad">
+                            +
+                            </button>
+                                <p>0</p>
+                            <button className="button-rad" >
+                            -
+                            </button>
+                        </div>
+                    </article>
+                    <article className="card-section1">
+                        <h2 className="title">-Título-</h2>
+                        <p className="subtitle">-Descripción-</p>
+                        <p className="subtitle">-Cantidad-</p>
+                        <p className="subtitle">Cómpralo a: </p>
+                        <p className="subtitle">Véndelo a: s/. </p>
+                        <p className="subtitle">Ganancia: s/.</p>
+                    </article>
+                    <section className='trash-icon'>
+                        <img src={trash} alt="trash-icon" className="trash-icon2" />
+                    </section>  
+                </section>   
+                    <hr className='line'></hr>
+                    <section className='total-container'>
+                       <p className='first-total'>Monto total: </p>
+                       <section className='pay'>s/ 200.00</section> 
+                    </section>
+                     <section className='total-container'>  
+                       <p className='discount-total'>Descuento de colaborador: </p>
+                       <section className='pay1'>- s/ 10.00</section>
+                    </section>
+                    <hr className='line'></hr>
+                    <section className='total-container'>
+                    <p className='final-total'>Total a pagar:</p> 
+                    <section className='pay'> s/ 180.00</section>
+                    </section>
+                    <section className='continue-container'>
+                        <bottom className='continue'>CONTINUAR</bottom>
+                    </section>
             </section>
         </section>  
+       </> 
     )
 }
 
